0d98c2fd73b8f9cfa484eed570bc4800c94ef6dc
Jikun <jikunchen@microsoft.com>
2023-05-29 14:23:22 +0800

    add " authorization" in headers of api

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,13 @@
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
+    // context.res.json({
+    //     text: "Hello from the API"
+    // });
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        headers: {
+            "Authorization": "Bearer some-token"
+        },
+        body: "Hello from Azure Static Web Apps API"
+    };
 };
